ef5a7f352c5aa5f20f431f7b11c35496ca407ec6
TheChickenHat <56360993+TheChickenHat@users.noreply.github.com>
2020-01-24 14:30:31 -0500

    Small Edits
    Coin deletion 2.0 (autodelete)
    Added the prefabs from design folder

---
+++ CombinedCodeTest/Assets/Scripts/AutoCoinRemover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoCoinRemover : MonoBehaviour
+{
+    private float DeletTime = 15.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(DeletTime);
+        DeletTime -= Time.deltaTime;
+
+        if (DeletTime <= 0.0f)
+        {
+            Destroy(gameObject, 2);
+            Debug.Log("Deleted");
+        }
+    }
+}
