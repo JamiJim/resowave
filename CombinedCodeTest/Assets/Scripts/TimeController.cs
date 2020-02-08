a35c787d502d5b70ac2f1b00ad4fef3a29d2bff1
TheChickenHat <56360993+TheChickenHat@users.noreply.github.com>
2020-02-08 12:24:31 -0500

    Game Restart, Some Code Edits, and Others
    Added functionality for game to reset after a set amount of time. Fixed Road Despawing Early Issue, Fixed an issue where the terrains wouldn't delete, fixed an issue where test coins could not be picked up, changed coin deletion to work on an invoke rather than update for performance.

--- CombinedCodeTest/Assets/Scripts/TimeController.cs
+++ CombinedCodeTest/Assets/Scripts/TimeController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class TimeController : MonoBehaviour
@@ -7,11 +8,13 @@ public class TimeController : MonoBehaviour
     #region Monobehavior API
     //Sets the paused state to false immediately
     private bool paused = false;
+    public float TimeStop = 30;
 
 
     // Update is called once per frame
     void Update()
     {
+        TimeStop -= Time.deltaTime;
         if (Input.GetKeyDown(KeyCode.E))
         {
             if (paused)
@@ -21,6 +24,18 @@ public class TimeController : MonoBehaviour
 
             paused = !paused;
         }
+
+        if (TimeStop <= 0)
+        {
+            Time.timeScale = 0;
+            StartCoroutine("GameReset", 10f);
+        }
+    }
+
+    IEnumerator GameReset()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        yield return new WaitForEndOfFrame();
     }
 
     #endregion
