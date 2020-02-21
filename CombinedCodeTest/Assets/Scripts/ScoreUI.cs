7842cac0d623926bcb3e364ce66bebb0fb7523b9
TheChickenHat <56360993+TheChickenHat@users.noreply.github.com>
2020-02-21 15:14:03 -0500

    Revert "Speedometer"
    This reverts commit 0252e502b0da5b7e9318f55747fe25aa2b62d47d.

--- CombinedCodeTest/Assets/Scripts/ScoreUI.cs
+++ CombinedCodeTest/Assets/Scripts/ScoreUI.cs
@@ -16,6 +16,6 @@ public class ScoreUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        scoreText.text = gm.Score.ToString() + System.Environment.NewLine +"Score";
+        scoreText.text = gm.Score.ToString();
     }
 }
