5a880ecaa4a9c6b33676bd75a02493b43e727500
TheChickenHat <56360993+TheChickenHat@users.noreply.github.com>
2020-03-20 12:37:49 -0400

    Game stops if you don't move
    If you don't move for about 10 seconds, the game enters a state where its paused but you can't resume it. Would like to see if having menu options fixes this.

--- CombinedCodeTest/Assets/Scripts/TimeController.cs
+++ CombinedCodeTest/Assets/Scripts/TimeController.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 
 public class TimeController : MonoBehaviour
 {
-    #region Monobehavior API
+    
     //Sets the paused state to false immediately
     private bool paused = false;
+    private float speed;
+    //private float StartTimer = 10f;
+    private float PauseTimer = 10f;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
 
 
     // Update is called once per frame
     void Update()
     {
+        speed = rb.velocity.magnitude;
+        Debug.Log(speed);
 
         if (Input.GetKeyDown(KeyCode.E))
         {
@@ -24,8 +35,35 @@ public class TimeController : MonoBehaviour
             paused = !paused;
         }
 
+        if (speed <= 10.0f)
+        {
+            PauseGame();
+            UnPauseGame();
+        }
+        //StartTimer -= Time.deltaTime;
+        /*if (StartTimer == 0)
+        {
+
+        }*/
+
     }
 
+ void PauseGame()
+  {
+     PauseTimer -= Time.deltaTime;
+     if (PauseTimer <= 0)
+     {
+        Time.timeScale = 0;
+            
+     }
+  }
+   
+  void UnPauseGame()
+  {
+    if (Input.GetKeyDown(KeyCode.R))
+    {
+        //Unpause game here
+    }
+  }
 
-    #endregion
 }
