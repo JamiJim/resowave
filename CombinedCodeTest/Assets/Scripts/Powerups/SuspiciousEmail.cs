059d0db26b56e6eed09076820c9b59d15df6a433
Owen <56360779+OwenPlusWall@users.noreply.github.com>
2020-03-25 14:30:46 -0400

    Many Changes to Power-Ups (See Patch Notes Below)
    -Net:
    -The Net has been split apart, into a "Left Net" and "Right Net." The old version of the Net that contains both sides has been renamed to "Double Net," just in case. When noting the Left and Right Nets, both will simply be referred to as "Net."
    -Transform properties have been changed, making the Net more visible to the player during gameplay.
    -Visual effects for the Net appearing and disappearing have been added.
    -The timer now resets to its "Lifetime" value when a Net of the same type is collected while one is already active.
    
    -Virus:
    -The score decrement value has now been attributed to the VZPlayer script. This lets the Suspicious Email activate the effect more easily, as well.
    
    -Suspicious Email:
    -Now has a chance to activate the effect of a Virus pickup, in addition to all other implemented Power-Ups.
    -The Left and Right Nets have been added to this list separately.
    
    -Step-Up Converter:
    -The timer now resets to its "Lifetime" value when a Step-Up Converter is collected while one is already active.
    
    -/Seek:
    -The timer now resets to its "Lifetime" value when a /Seek is collected while one is already active.

--- CombinedCodeTest/Assets/Scripts/Powerups/SuspiciousEmail.cs
+++ CombinedCodeTest/Assets/Scripts/Powerups/SuspiciousEmail.cs
@@ -5,13 +5,34 @@ using UnityEngine;
 public class SuspiciousEmail : MonoBehaviour
 {
     public GameObject[] Effects;
+    private VZPlayer Player;
+    private int RandomRange;
     //public AudioSource aSource;
     //public AudioClip aSound;
 
-
+    private void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("BikeInputCont").GetComponent<VZPlayer>();
+    }
     private void PowerupEffect()
     {
-        Instantiate(Effects[Random.Range(0, (Effects.Length - 1))], this.transform.position, Quaternion.identity); //A random powerup effect is used, from a list specified in the editor.
+        RandomRange = Random.Range(0, (Effects.Length + 1));
+        if (RandomRange >= Effects.Length) //There's a chance that the Suspicious Email will act like a virus, rather than like a Power-Up.
+        {
+            if (Player.Score < Player.VirusDecrement)
+            {
+                Player.Score = 0;
+            }
+            else
+            {
+                Player.Score -= Player.VirusDecrement;
+            }
+            Debug.Log("Virus!");
+        }
+        else {
+            Instantiate(Effects[RandomRange], this.transform.position, Quaternion.identity); //A random powerup effect is used, from a list specified in the editor.
+            Debug.Log("Power-Up!");
+        }
         //aSource.PlayOneShot(aSound, 1.0f);
         Destroy(this.gameObject);
     }
