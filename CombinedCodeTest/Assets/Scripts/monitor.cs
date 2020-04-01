40cfdc963fd05b66b6ac6d485dcb5840b048308a
elijahbenjamin1998 <56360868+elijahbenjamin1998@users.noreply.github.com>
2020-04-01 13:10:26 -0400

    Skybox Fix

---
+++ CombinedCodeTest/Assets/Scripts/monitor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class monitor : MonoBehaviour
+{
+    public GameObject bike;
+    public GameObject CPUmonitor;
+    float timer = 0.0f;
+    int seconds;
+    void Start() 
+    {
+        invokeRepeating("Rise",0f,30f);
+        invokeRepeating("Fall", 20f, 30f);
+    }
+    void Update()
+    {
+        timer += Time.deltaTime%60;
+        Debug.Log(timer+"seconds");
+        Vector3 temp = new Vector3(-100, 50, 200f);
+        CPUmonitor.transform.position = bike.transform.position+temp;
+        
+    }
+    void Rise() { }
+}
---
+++ CombinedCodeTest/Assets/Scripts/monitorMessages.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class monitorMessages : MonoBehaviour
+{
+    public Text message;
+    string messagechoice;
+    int choiceCount = 1;
+    void Start()
+    {
+        InvokeRepeating("messageCycle", 0.0f, 30.0f);
+
+    }
+    void Update()
+    {
+        message.text = messagechoice;
+    }
+    void messageCycle()
+    {
+        switch (choiceCount)
+        {
+            case 1:
+                messagechoice = "Let's ride!";
+                choiceCount++;
+                break;
+
+            case 2:
+                messagechoice = "You're doing great!";
+                choiceCount++;
+                break;
+            case 3:
+                messagechoice = "~You're a resonation sensation~";
+                choiceCount++;
+                break;
+
+            case 4:
+                messagechoice = "Keep it up Zoombie!";
+                choiceCount++;
+                break;
+            case 5:
+                messagechoice = "Vaporizing Speeds";
+                choiceCount = 1;
+                break;
+        }
+
+    }
+}
