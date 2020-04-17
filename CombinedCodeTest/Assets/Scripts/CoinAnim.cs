a53097c1d561e2c5b48ee5412b9a0d3f36181323
TheChickenHat <56360993+TheChickenHat@users.noreply.github.com>
2020-04-17 13:44:03 -0400

    Coin Animations
    Coin Animations should be up and running

--- CombinedCodeTest/Assets/Scripts/CoinAnim.cs
+++ CombinedCodeTest/Assets/Scripts/CoinAnim.cs
@@ -1,48 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
+// Makes objects float up & down while gently spinning.
 public class CoinAnim : MonoBehaviour
 {
-    public float DelayTimer = 3f;
-    // Start is called before the first frame update
+    // User Inputs
+    public float degreesPerSecond = 15.0f;
+    public float amplitude = 0.5f;
+    public float frequency = 1f;
+
+    // Position Storage Variables
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+
+    // Use this for initialization
     void Start()
     {
- 
+        // Store the starting position & rotation of the object
+        posOffset = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        GoUp();
 
-    }
+        // Float up/down with a Sin()
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
 
-  void GoUp()
-  { 
-      for (int i = 0; i < 10; i++)
-      {
-            if (i == 5)
-            {
-                GoDown();
-
-            }
-            DelayTimer -= Time.deltaTime;
-            this.gameObject.transform.position = transform.position + new Vector3(0f, 0.01f, 0f);
-
-      }
-
-
-    }
-
-    void GoDown()
-    {
-        for (int i = 0; i < 10; i++)
-        {
-            DelayTimer -= Time.deltaTime;
-            this.gameObject.transform.position = transform.position + new Vector3(0f, -0.01f, 0f);
-        }
-
-
+        transform.position = tempPos;
     }
 }
