59b4f47e7cb99c0cc9d2416268dd523938c0f066
Owen <56360779+OwenPlusWall@users.noreply.github.com>
2020-04-20 19:27:46 -0400

    Revert "Revert "Implemented "Settings" Option in Main Menu""
    This reverts commit 1dba2cdbb1e1f6841b507a69ab227380c9a2cdd6.

---
+++ CombinedCodeTest/Assets/Scripts/Menu/EmptySlider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EmptySlider : MonoBehaviour
+{
+    private Slider Amount;
+    private GameObject Fill;
+    public string ChildFillArea;
+
+    private void Start()
+    {
+        Amount = this.gameObject.GetComponent<Slider>();
+        Fill = GameObject.Find(ChildFillArea);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Amount.value <= 0)
+        {
+            Fill.SetActive(false);
+        }
+        else
+        {
+            Fill.SetActive(true);
+        }
+    }
+}
